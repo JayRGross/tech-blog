32f7c2200485fd0e2c6deb9d820cc4f6244e589c
Jaylin Gross <grossjaylin@gmail.com>
2021-12-30 11:44:22 -0500

    redo

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./postRoutes');
-
-router.use('/users', userRoutes);
-router.use('/post', postRoutes);
-
-module.exports = router;
